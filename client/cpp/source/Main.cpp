1b97a1d1303d63a8e20a381f02b56b97b897efdf
dbinder <dylan.binder@gmail.com>
2016-04-21 23:41:29 -0700

    ToHexString.

--- client/cpp/source/Main.cpp
+++ client/cpp/source/Main.cpp
@@ -1,6 +1,7 @@
 #include <string>
 #include <iostream>
 #include <Array.h>
+#include <fstream>
 
 void removeSpaces(char *inString)
 {
@@ -29,12 +30,57 @@ void removeSpaces(char *inString)
   *start = 0;
 }
 
+struct Vector3
+{
+  float m_x;
+  float m_y;
+  float m_z;
+  static const Vector3 GRAVITY;
+  static const Vector3 ORIGIN;
+  static const Vector3 X;
+  static const Vector3 Y;
+  static const Vector3 Z;
+
+  float operator()(const int index)
+  {
+    return (&m_x)[index];
+  }
+  
+  friend std::istream& operator>>(std::istream& input, Vector3& lhs)
+  {
+    input >> lhs.m_x
+      >> lhs.m_y
+      >> lhs.m_z;
+    return input;
+  }
+};
+
+std::string ToHexString(const char* hex)
+{
+  std::string tmp;
+  const char *hexAlpha = "0123456789ABCDF";
+  for (int i = 0; i < strlen(hex); ++i)
+  {
+    //tmp.append(hexAlpha[hex[i] >> 4]);
+    tmp += (*hexAlpha + (hex[i] >> 4));
+    tmp += (*hexAlpha + (hex[i] & 0xF));
+  }
+  return tmp;
+}
+
 int main(int argc, char* argv[])
 {
+  std::ifstream inFile;
+  inFile.open("test.txt", std::ios_base::in);
   char test[256] = { "A string with spaces" };
   char test2[256] = { "A  string  with  more           spaces" };
   Array<int> temp(10);
-
+  const char *byte = "THISISTRYHARD";
+  ToHexString(byte);
+  Vector3 vec;
+  inFile >> vec;
+  vec.m_x = 1;
+  std::cout << vec(0);
   std::cout << temp.GetLength();
   
   std::cout << strlen(test);
